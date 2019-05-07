4fef91112d369ba69c886985004ce8e0a6923c69
owezzy <owenobezzy@gmail.com>
2019-05-07 19:04:10 +0300

    add cors and swagger doc generator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const appOptions = {cors: true};
+    const app = await NestFactory.create(AppModule, appOptions);
+    app.setGlobalPrefix('api');
+
+    const options = new DocumentBuilder()
+        .setTitle('soko-bora API')
+        .setDescription('An API interface to manage a Grocery Store')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('/docs', app, document);
+
+    await app.listen(3000);
 }
+
 bootstrap();
