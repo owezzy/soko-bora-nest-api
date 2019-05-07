73756f1168467c77c9a63138986790d6d9f05937
owezzy <owenobezzy@gmail.com>
2019-05-07 19:06:28 +0300

    import UserModule and typeOrm to app instance

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {UserModule} from "./user/user.module";
+import {Connection} from "typeorm";
+
+
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot(),
+      UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private readonly connection: Connection){}
+}
