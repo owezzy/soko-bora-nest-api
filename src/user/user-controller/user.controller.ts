fe045e30923075ac0a2a7ab6d9e7efc5edf8b43f
owezzy <owenobezzy@gmail.com>
2019-05-07 19:01:50 +0300

    database connection file

---
+++ src/user/user-controller/user.controller.ts
@@ -0,0 +1,13 @@
+import {Controller, Get} from '@nestjs/common';
+import {UserService} from "./user.service";
+import {User} from "./user-model/user.interface";
+
+@Controller()
+export class UserController {
+    constructor (private readonly userService: UserService) {}
+
+    @Get('user')
+    async findMe(@User){
+
+    }
+}
